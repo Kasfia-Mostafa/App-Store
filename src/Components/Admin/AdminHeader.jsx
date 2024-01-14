f131ca53636f685b15b85617645df4b1cdf06742
SwornaCode <kasfiamostafa03@gmail.com>
2024-01-14 13:01:25 +0600

    sign in and out

--- src/Components/Admin/AdminHeader.jsx
+++ src/Components/Admin/AdminHeader.jsx
@@ -1,11 +1,24 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Containers/Providers/AuthProvider";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { FaChevronDown } from "react-icons/fa6";
+import { Menus } from "../../Utils/Menus";
 
 const AdminHeader = () => {
   const { user, logOut } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    logOut()
+      .then((result) => {
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error.massage);
+      });
+  };
 
   return (
     <div className="w-full flex justify-between items-center">
@@ -47,7 +60,7 @@ const AdminHeader = () => {
         </div>
         {/* image content */}
         <div>
-          <div className="w-14 h-14 rounded-full p-1 flex justify-center items-center relative bg-gradient-to-b from-heroPrimary to bg-heroSecondary">
+          <div className="w-16 h-16 rounded-full p-1 flex justify-center items-center relative bg-gradient-to-b from-heroPrimary to bg-heroSecondary">
             {user ? (
               <img
                 className="w-full h-full object-cover rounded-full"
@@ -61,15 +74,51 @@ const AdminHeader = () => {
                 src="https://i.postimg.cc/0y0C2TGn/avatar.jpg"
               />
             )}
-             <div className="w-4 h-4 rounded-full bg-secondary absolute bottom-1 right-1 flex justify-center items-center border border-gray-600">
-            <FaChevronDown className="text-[10px] text-textSecondary"></FaChevronDown>
-          </div>
+            <div className="w-4 h-4 rounded-full bg-secondary absolute bottom-1 right-1 flex justify-center items-center border border-gray-600">
+              <FaChevronDown className="text-[10px] text-textSecondary"></FaChevronDown>
+            </div>
 
-          {/* user dropdown */}
-          
+            {/* user dropdown */}
+            <div className="absolute top-20 right-0 bg-secondary w-64 flex flex-col justify-start itm-start px-3 py-2 gap-4 rounded-md z-50">
+              {Menus &&
+                Menus.map((menu) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <React.Fragment>
+                    {menu.isAdmin ? (
+                      <Link
+                        className="px-1 py-2 font-semibold hover:text-heroSecondary"
+                        key={menu.id}
+                      >
+                        {menu.menu}
+                      </Link>
+                    ) : (
+                      <Link
+                        className="px-1 py-2 font-semibold hover:text-heroSecondary"
+                        key={menu.id}
+                      >
+                        {menu.menu}
+                      </Link>
+                    )}
+                  </React.Fragment>
+                ))}
+
+              {/* sign in and out */}
+              {user ? (
+                <button
+                  onClick={handleSignOut}
+                  className="px-4 py-3 w-full rounded-md text-primary bg-textPrimary active:scale-95 transition-all ease-in-out"
+                >
+                  Sign out
+                </button>
+              ) : (
+                <Link to="/login">
+                  <button className="px-4 py-3 w-full rounded-md text-primary bg-textPrimary active:scale-95 transition-all ease-in-out">
+                    Sign in
+                  </button>
+                </Link>
+              )}
+            </div>
           </div>
-         
-         
         </div>
       </div>
     </div>
---
+++ src/Utils/Menus.jsx
@@ -0,0 +1,7 @@
+export const Menus = [
+  {id: 1001, menu: "My Profile", url: "/profile"},
+  {id: 1002, menu: "My Favorite", url: "/favorite"},
+  {id: 1003, menu: "My Apps", url: "/apps" , isAdmin : true},
+  {id: 1004, menu: "Dashboard", url: "/dashboard", isAdmin : true},
+  {id: 1005, menu: "Users", url: "/users" , isAdmin : true},
+]
